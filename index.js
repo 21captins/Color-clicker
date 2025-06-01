cc0393a3aa94bdc7e5f576fe60c02026af7129ef
21captins <167474367+21captins@users.noreply.github.com>
2025-06-01 09:26:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const textColors = ["#25618a", "#397ba6", "#4b8c7b", "#c49a6c", "#b85c2b"];
 
 
 document.querySelector(".color-btn").addEventListener("click",function(){
-    const randomIndex = Math.floor(Math.random()*colors.length)
-    document.body.style.backgroundColor = colors[randomIndex];
+    const r = Math.floor(Math.random()*256)
+    const g = Math.floor(Math.random()*256)
+    const b = Math.floor(Math.random()*256)
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     const h4 = document.querySelector("h4");
     h4.textContent = colorNames[randomIndex];
     h4.style.color = textColors[randomIndex];
